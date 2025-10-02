786134938aab1ae3434addaa8da6285c9466d784
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-02 14:39:24 +0530

    BruteF to move 0 to the end

---
+++ array_rev.cpp/moving_zero.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    //TC--->O(N)
+    //SC--->O(N)
+    vector<int> temp;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            cnt++;
+        }else{
+            temp.push_back(arr[i]);
+        }
+    }
+    for(int i=0;i<cnt;i++){
+        temp.push_back(0);
+    }
+    for(int i=0;i<temp.size();i++){
+        cout<<temp[i]<<" ";
+    }
+
+}

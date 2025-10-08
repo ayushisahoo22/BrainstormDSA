2b3ac236358d9ada9800f7bb96af551d015a19e3
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-08 22:54:58 +0530

    Minimum jumps to last element

--- array_rev.cpp/min_jumps.cpp
+++ array_rev.cpp/min_jumps.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //Jumping only to the fixed index
 //Test case failed at --> 2 3 1 1 4
 //output should be 2 mor min jumps and it is giving 3 which is index jump
-int indxJump(vector<int>& arr) {
+/*int indxJump(vector<int>& arr) {
         int cnt=0;
         int indx=0;
         while(indx<arr.size()-1){
@@ -16,9 +16,30 @@ int indxJump(vector<int>& arr) {
             cnt++;
         }
         return cnt;
-}
+}*/
 
-
+//TC-->O(N^2)
+int minJumps(vector<int>& arr) {
+        int cnt=0;
+        int indx=0;
+        if (arr[0] == 0) return -1;
+        while(indx<arr.size()-1){
+            int j=indx;
+            indx+=arr[indx];
+            int maxi=arr[j+1];
+            int best=j+1;
+            for(int i=j+1;i<=indx;i++){
+                if(maxi<arr[i]){
+                    maxi=arr[i];
+                    best=i;
+                }
+            }
+            indx = best;
+            if(arr[indx]==0) return -1;
+            cnt++;
+        }
+        return cnt;
+    }
 
 int main(){
     int n;
@@ -28,6 +49,9 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int ans= indxJump(arr);
+    // int ans= indxJump(arr);
+    // cout<<ans;
+
+    int ans= minJumps(arr);
     cout<<ans;
 }

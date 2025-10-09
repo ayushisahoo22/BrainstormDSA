e69da0d9674dc9e07bfa130f37bf7089bebf732d
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-09 23:10:09 +0530

    Kth smallest element in arr

--- array_rev.cpp/k_smallest.cpp
+++ array_rev.cpp/k_smallest.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int kthSmallest(vector<int>& v, int n, int k) {
-int k_small;
+int k_small;   // to update with the next smallest element
 while(k--){
     int smallest=INT_MAX;
-    int indx=-1;
+    int indx=-1;    //keeping track for index of smallest element
     for(int i=0;i<n;i++){
         if(v[i]<smallest && v[i]!=smallest){
             smallest=v[i];
             indx=i;
         }
     }
-    v[indx]=INT_MAX;
-    k_small=smallest;
+    v[indx]=INT_MAX;   //the smallest element is updated so it should not be considered next time
+    k_small=smallest;  //giving the value of smallest as each iteration initiate as smallest=INT_MAX
     }
     return k_small;
 }

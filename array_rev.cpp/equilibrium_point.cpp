a3795b5d822c5d8a4ddc641f1d3a6eed139f2c90
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-12 17:36:16 +0530

    Better approach for equilibrium point

--- array_rev.cpp/equilibrium_point.cpp
+++ array_rev.cpp/equilibrium_point.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 //TC---> O(N^2)
 
-int findEquilibrium(vector<int> &arr) {
+/*int findEquilibrium(vector<int> &arr) {
     // int indx=-1;
     for(int i=0;i<arr.size();i++){
         int sum1=0,sum2=0;
@@ -20,7 +20,37 @@ int findEquilibrium(vector<int> &arr) {
         }
     }
     return -1;  //if no i exists then return -1
+}*/
+
+
+//Optimal approach
+//Two pointer approch for positives only
+
+//It wont work for the array containing negative number bcz Negative numbers can reduce a sum.
+//So sum1 < sum2 at one step doesn’t guarantee that adding another number from the left moves us closer to equality
+//it might move us further away.
+//When all numbers are positive → sums always increase, so balance can only happen once.
+//When negatives exist → sums can oscillate, so you can skip over the equilibrium index completely.
+int findEquilibrium(vector<int> &arr) {
+    int n=arr.size();
+    int i=0;int j=n-1;
+    int sum1=0,sum2=0;
+    while(i<j){
+        if(sum1<sum2){
+            sum1+=arr[i];
+            i++;
+        }else{
+            sum2+=arr[j];
+            j--;
+        }
+    }
+    if(sum1==sum2){
+        return i;
+    }
+    return -1;
 }
+
+
 int main(){
     int n;
     cout<<"Enter the value of n: ";

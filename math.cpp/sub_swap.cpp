531005c9271c8db3620328f8a992544a265aad21
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-27 23:37:02 +0530

    Subtract and swap problem

---
+++ math.cpp/sub_swap.cpp
@@ -0,0 +1,31 @@
+// given two positive integers X and Y.
+// Repeat the following operation until X is not 0:
+// If X>Y, swap X and Y;
+// Otherwise, set X=(Y−X) and Y=X.
+// Output the final value of Y after all operations.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int temp;
+	    while(x!=0){
+	        if(x>y){
+	            temp=x;
+	            x=y;
+	            y=temp;
+	        }else{
+	            temp=x;
+	            x=y-x;
+	            y=temp;
+	        }
+	    }
+	    cout<<y<<endl;
+	}
+
+}

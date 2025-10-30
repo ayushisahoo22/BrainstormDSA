d3211d05f46bba792d85f4defbe3f6b820545aea
ayushisahoo22 <ayushiis0912@gmail.com>
2025-10-30 22:44:19 +0530

    Problem to rearrange array elements

---
+++ array_rev.cpp/rearrange_array.cpp
@@ -0,0 +1,8 @@
+// Given an array ‘A’ having ‘N’ integers and an integer ‘m’. You need to rearrange the array elements such that after re-arrangement difference of array elements with ‘m’ should be in a sorted fashion.
+
+// If the difference between any two array elements and ‘m’ is the same, then the element which comes first in the given array will be placed first in the re-arranged array.
+
+// For Example
+// If we have A =  [3, 5, 7, 9, 2, 6]  and m = 5.
+// Difference of array elements with m : [2, 0, 2, 4, 3, 1].
+// Array after rearrangement : [5, 6, 3, 7, 2, 9].

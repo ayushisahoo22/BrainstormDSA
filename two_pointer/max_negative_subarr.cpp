f224744a862960468db9f205fe44cbe18ab77038
ayushisahoo22 <ayushiis0912@gmail.com>
2025-12-16 23:37:33 +0530

    Max length of -ve subarr (1 pointer)

---
+++ two_pointer/max_negative_subarr.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//find maximum length of subarray containing negative integers
+//eg->{-3,-9,0,-4,-5,-6,7} ans will be 3 as max length is of
+// -4,-5,-6 
+
+//One pointer approach
+int maxNegative(vector<int> arr,int n){
+    int len=0;
+    int curr=0;
+    for(int j=0;j<n;j++){
+        if(arr[j]<0){
+            curr++;
+            len=max(len,curr);
+        }else{
+            curr=0;
+        }
+    }
+    return len;
+}
+
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans=maxNegative(arr,n);
+    cout<<ans;
+}

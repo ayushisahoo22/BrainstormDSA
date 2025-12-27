0a718c33763cfcd1e0892d0c76d6f1468896b7aa
ayushisahoo22 <ayushiis0912@gmail.com>
2025-12-27 19:05:26 +0530

    Maximum Sum Subarray of Size K

---
+++ sliding_window/max_subarr_k.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Given an array of integers arr and an integer k
+// find the maximum sum of any contiguous subarray of size k.
+
+int maxLen(vector<int>arr,int k){
+    int maxi=0;
+    int i=0;
+    int sum=0;
+    for(int j=0;j<arr.size();j++){
+        sum+=arr[j];
+        if(j-i+1>k){
+            sum-=arr[i];
+            i++;
+        }
+        maxi=max(sum,maxi);
+    }
+    return maxi;
+}
+
+int main() {
+    vector<int>arr;
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    int k;
+    cout<<"Enter k: ";
+    cin>>k;
+    int ans=maxLen(arr,k);
+    cout<<ans;
+}

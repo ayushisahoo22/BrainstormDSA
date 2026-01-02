c5730355442a74f3a3ffda9acfcc183c037e6d14
ayushisahoo22 <ayushiis0912@gmail.com>
2026-01-02 18:18:58 +0530

    Maximum ele in k sized window (dq)

--- sliding_window/maximum_ele.cpp
+++ sliding_window/maximum_ele.cpp
@@ -5,20 +5,46 @@ using namespace std;
 // Return an array containing the maximum element in every contiguous subarray of size k.
 
 //using pq---TC=O(NlogN)
+// vector<int> maxInWindow(vector<int>arr,int k){
+//     priority_queue<pair<int,int>> pq;
+//     vector<int>ans;
+//     int i=0;
+//     for(int j=0;j<arr.size();j++){
+//         pq.push({arr[j],j});
+//         if(j-i+1>k){
+//             i++;
+//         }
+//         if(j-i+1==k){
+//             while(!pq.empty() && pq.top().second<i){
+//                 pq.pop();
+//             }
+//             ans.push_back(pq.top().first);
+//         }
+//     }
+//     return ans;
+// } 
+
+//using deque---TC=O(N)
 vector<int> maxInWindow(vector<int>arr,int k){
-    priority_queue<pair<int,int>> pq;
+    deque<int> dq;
     vector<int>ans;
     int i=0;
     for(int j=0;j<arr.size();j++){
-        pq.push({arr[j],j});
-        if(j-i+1>k){
-            i++;
+        //Remove smaller elements (maintain decreasing order)
+        while(!dq.empty() && arr[dq.back()]<arr[j]){
+            dq.pop_back();
+        }
+
+        //pushing element
+        dq.push_back(j);
+
+        //after window slides if max is not in the curr window
+        while(!dq.empty() && dq.front()<i){
+            dq.pop_front();
         }
         if(j-i+1==k){
-            while(!pq.empty() && pq.top().second<i){
-                pq.pop();
-            }
-            ans.push_back(pq.top().first);
+            ans.push_back(arr[dq.front()]);
+            i++;
         }
     }
     return ans;
